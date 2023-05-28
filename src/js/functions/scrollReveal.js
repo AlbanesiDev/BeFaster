b16db98d76269617b53526b1fb21c1864dd2186d
AlbanesiDev <joaquinalbanesidev@gmail.com>
2023-05-28 20:04:01 -0300

    ScrollReveal v1.0

---
+++ src/js/functions/scrollReveal.js
@@ -0,0 +1,10 @@
+ScrollReveal({ distance: '500px' }).reveal(".revealImage", {
+    viewOffset: {
+        bottom: 10
+    },
+    easing: "ease-out", 
+    origin: 'bottom',
+    delay: 100,
+    duration: 600,
+    interval: 300, 
+})
